46edf5ddd159aeca0a307cc2f98745a10c7cebd7
albatro33 <cokujin518@gmail.com>
2022-11-29 16:20:10 +0900

    change markdown plugin setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
 import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-import test from './posts/test.md';
+import {MDComponent} from './posts/test.md';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [content, setContent] = useState('');
-
-  const readMD = ()=>{
-    fetch(test).then(res=>res.text()).then(text=>setContent(text));
-  }
-
+  
   return (
     <div className="App">
       <p>hello albatro33</p>
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <MDComponent />
     </div>
   )
 }
--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,1 +1,6 @@
-declare module '*.md'
+declare module '*.md'{
+    const attributes: Record<string, unknown>
+    import React from 'react'
+    const MDComponent: React.VFC
+    export {attributes, MDComponent};
+}
