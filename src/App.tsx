b0e7415726c05c4fc5dad43b2a1a5a98b2e4420f
albatro33 <cokujin518@gmail.com>
2022-11-29 14:48:39 +0900

    markdown test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import test from './posts/test.md';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [content, setContent] = useState('');
+
+  const readMD = ()=>{
+    fetch(test).then(res=>res.text()).then(text=>setContent(text));
+  }
 
   return (
     <div className="App">
-        <p>hello albatro33</p>
+      <p>hello albatro33</p>
+      <ReactMarkdown>{content}</ReactMarkdown>
     </div>
   )
 }
